fb60402d9abcb08815fbf68ca3edea214a6b4472
Lucas <l.duarte.mk@gmail.com>
2019-08-26 14:07:00 -0300

    hash password with bcrypt

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
 import UserController from './app/controllers/UserController';
 
 const routes = new Router();
-
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Oi',
-    email: 'oi@oi',
-    password_hash: '12',
-  });
-
-  return res.json(user);
-});
 
 routes.post('/users', UserController.store);
 
