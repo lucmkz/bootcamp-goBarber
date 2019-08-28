de909813dbc153f2ae164e37198a89b528dcc6ea
Lucas <l.duarte.mk@gmail.com>
2019-08-28 14:10:19 -0300

    rules for scheduling

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import User from '../models/User';
 import Appointment from '../models/Appointment';
 
@@ -23,6 +24,24 @@ class AppointmentController {
       return res.status(401).json({
         error: 'You can only create appointments with a provider user',
       });
+    }
+
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are no pertided' });
+    }
+
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return res.status(400).json({ error: 'De date is not availability' });
     }
 
     const appointment = await Appointment.create({
