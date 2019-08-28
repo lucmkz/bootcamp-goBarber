f62e28b20eae26b963ea190a3f5398c6e649729f
Lucas <l.duarte.mk@gmail.com>
2019-08-28 11:59:32 -0300

    create appointment api and migrations

--- src/database/migrations/20190828124929-add-avatar-field-to-user.js
+++ src/database/migrations/20190828124929-add-avatar-field-to-user.js
@@ -1,16 +1,5 @@
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.addColumn('users', 'avatar_id', {
-      type: Sequelize.INTEGER,
-      references: {
-        model: 'files',
-        key: 'id',
-        onUpdate: 'CASCADE',
-        onDelete: 'SET NULL',
-        allowNull: true,
-      },
-    });
-  },
+  up: (queryInterface, Sequelize) => {},
 
   down: queryInterface => {
     return queryInterface.removeColumn('users', 'avatar_id');
